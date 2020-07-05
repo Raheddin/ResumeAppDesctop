aa301a3c00f89b835c6f450358581eb464886f28
Raheddin <61094582+Raheddin@users.noreply.github.com>
2020-07-06 02:27:58 +0400

    WEB-den evelki versiya

---
+++ ResumeDesktopApp/src/main/java/com/company/resume/config/Config.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.company.resume.config;
+
+import com.Company.entity.User;
+
+/**
+ *
+ * @author acer
+ */
+public class Config {
+    public static User loggedInUser;
+}
